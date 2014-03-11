b1526796c036a3a2cc364f205bf54c6e24dd0b5e
VrijVlinder <adalib@mac.com>
2014-03-11 11:34:39 -0600

    New

--- container.js
+++ container.js
@@ -1,6 +1,6 @@
  $(document).ready(function() {
 
-$('#Body').prepend('<iframe style="width:98.8%;height:700px;" src="/foro/plugins/Frame/frame.html"></iframe>');
+$('#Body').prepend('<iframe style="width:98.8%;height:700px;" src="/plugins/Container/container.html"></iframe>');
 
 //If your forum is not in the root please add the name of it's directory in the url above or the full url to this file
 // If you want the radio before everything in the panel use .prepend instead of .append . If you want to add it somewhere specific add the name and class to the #Panel part for example '#Panel #Bookmarks.Box' . You can carefully remove the link in the code above if you want.
